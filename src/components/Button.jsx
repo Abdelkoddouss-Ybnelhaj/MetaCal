47bd12db20ff0e25b02b8e47a0cf2c72f13cc5b2
Abduh-Belhaje <abduhbelhaj@gmail.com>
2024-07-26 11:42:13 +0100

    button component added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 function Button(props) {
   return (
-    <div>
-      <span className= {`cursor-pointer text-${props.size} py-${props.py} text-${props.color} border-2 bg-${props.bg} border-blue-400 px-${props.px} rounded`}>{props.title}</span>
-    </div>
-    
+      <button className= {`cursor-pointer text-${props.size} text-${props.color} bg-gray-200`}>{props.title}</button>
   )
 }
 
