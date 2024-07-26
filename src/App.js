7ccb43f3ea43c7633ad65a2032aaf16e27827068
Abduh-Belhaje <abduhbelhaj@gmail.com>
2024-07-26 11:51:51 +0100

    making the Home page resp

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Home from "./pages/Home"
 import CalorieCal from "./pages/CaloriesCal"
 import Meals from "./pages/Meals"
 import NavBar from "./components/NavBar";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (  
-    <div className="px-40">
-      <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/Calories" element={<CalorieCal />}/>
-          <Route path="/Meals" element={<Meals />}/>
-        </Routes>
-      </BrowserRouter>
+    <div className="lg:px-40 md:px-20 px-14 bg-bg_color h-screen ">
+      <UserProvider>
+        <NavBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/Calories" element={<CalorieCal />}/>
+            <Route path="/Meals" element={<Meals />}/>
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import Diet from '../assets/images/Diet.png'
 import { IoMdArrowForward } from "react-icons/io";
-import {redirect} from '../utils/RouteUtils'
+import {redirect} from '../utils/Helper'
 
 function Home() {
   return (
-    <div className='flex justify-between'>
-      <div className='pt-28 '>
+    <div className='lg:pt-10 md:pt-10 pt-2 flex justify-between sm:justify-center'>
+      <div className='pt-20 lg:w-1/2 md:w-1/2 sm:w-full'>
         <div className='flex flex-col w-4/5 '>
-          <span className='text-3xl pb-7 text-gray-800'>“Calculate your daily calorie needs and create customized meal plans.”</span>
-          <span className='text-xs text-gray-500 w-4/5'>Achieve your health goals with our personalized nutrition plans tailored to your needs. Our expert-guided plans help you make informed dietary choices.</span>
+          <span className='lg:text-4xl md:text-3xl font-medium pb-7 text-gray-700 text-4xl'>“Calculate your daily calorie needs and create customized meal plans.”</span>
+          <span className='lg:text-xs text-ls text-gray-500 '>Achieve your health goals with our personalized nutrition plans tailored to your needs. Our expert-guided plans help you make informed dietary choices.</span>
         </div>
         
         <div className='flex pt-10 items-center'>
-        <span className= {`cursor-pointer text-md py-2 text-white bg-primaryColor px-10 rounded`} onClick={()=> redirect('Calories')}>Get Started</span>
-          <div className='flex item-center hover:text-primaryColor duration-200' onClick={()=> redirect('Meals')}>
-            <span className='pl-5 cursor-pointer pr-3'>Explore Nutrition Plans </span>
-            <IoMdArrowForward size="25" />
+        <span className= {`cursor-pointer lg:text-md md:text-sm text-xs py-2 text-white bg-primaryColor lg:px-10 md:px-5 sm:px-5 px-3 rounded`} onClick={()=> redirect('Calories')}>Get Started</span>
+          <div className='flex items-center hover:text-primaryColor duration-200  ' onClick={()=> redirect('Meals')}>
+            <span className='pl-5 cursor-pointer pr-3 lg:text-md md:text-sm text-xs'>Explore Nutrition Plans </span>
+            <IoMdArrowForward size="18"/>
           </div>
         </div>
       </div>
 
-      <img src={Diet} alt=""  className='w-1/2'/>
+      <div className='w-1/2 lg:flex md:flex items-center justify-center hidden'>
+        <img src={Diet} alt=""  className='object-cover w-5/6'/>
+      </div>
     </div>
   )
 }
